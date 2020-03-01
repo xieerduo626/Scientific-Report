46c1f07eadfbda62fcd01c796bf3e1b1f725e39e
xieerduo626 <35492866+xieerduo626@users.noreply.github.com>
2020-02-29 18:39:54 -0600

    Update function.py

--- DataProcess/Matlibplot/function.py
+++ DataProcess/Matlibplot/function.py
@@ -1,3 +1,3 @@
 #set xsticks
-xticks([-2.5 -1 0 1 2.5]) #set specific value
+plt.xticks([-2.5, -1, 0, 1, 2.5],["Jan\n2009", "Feb\n2009", "Mar\n2009", "Apr\n2009", "May\n2009"]) #set specific value
 plt.xticks(np.arange(5),('before 180nm', '180nm,130nm', '90nm,65nm', '45nm,32nm,22nm', '14nm & after'))
