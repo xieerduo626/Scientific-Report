ec6ba31ac9a973788337c6d5e99180dde0376828
xieerduo626 <35492866+xieerduo626@users.noreply.github.com>
2020-05-31 18:23:34 -0500

    Update function.py

--- DataProcess/Matlibplot/function.py
+++ DataProcess/Matlibplot/function.py
@@ -1,3 +1,7 @@
 #set xsticks
 plt.xticks([-2.5, -1, 0, 1, 2.5],["Jan\n2009", "Feb\n2009", "Mar\n2009", "Apr\n2009", "May\n2009"]) #set specific value
 plt.xticks(np.arange(5),('before 180nm', '180nm,130nm', '90nm,65nm', '45nm,32nm,22nm', '14nm & after'))
+
+#import
+a=np.loadtxt('raman1_data/Q1 Dark Wavelength.csv', delimiter=",", skiprows=18,max_rows=1043)
+x=a[:,0]
